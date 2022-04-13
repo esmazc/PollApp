e91cb52292db4f498acc8f20cb2fb3441a2866e0
esmazc <ezejnilovi1@etf.unsa.ba>
2022-04-13 10:34:50 +0200

    commit

--- app/src/main/java/ba/etf/rma22/projekat/data/repositories/KorisnikRepository.kt
+++ app/src/main/java/ba/etf/rma22/projekat/data/repositories/KorisnikRepository.kt
@@ -1,8 +1,11 @@
 package ba.etf.rma22.projekat.data.repositories
 
 import ba.etf.rma22.projekat.data.models.Korisnik
+import ba.etf.rma22.projekat.data.user
 
 object KorisnikRepository {
-    val korisnik : Korisnik = Korisnik(arrayListOf(Pair("Istraživanje broj 1", "Grupa2"), Pair("Istraživanje broj 5", "Grupa1")))
 
+    fun getUser(): Korisnik {
+        return user()
+    }
 }
